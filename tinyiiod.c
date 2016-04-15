4b7d7b83aabe6f35caf8a41f53c427f664b3f24c
Paul Cercueil <paul.cercueil@analog.com>
2016-04-15 15:33:36 +0200

    Add a \0 at the end of the buffer to be written to an attribute
    Signed-off-by: Paul Cercueil <paul.cercueil@analog.com>

--- tinyiiod.c
+++ tinyiiod.c
@@ -161,6 +161,7 @@ void tinyiiod_do_write_attr(struct tinyiiod *iiod, const char *device,
 		bytes = sizeof(buf);
 
 	tinyiiod_read(iiod, buf, bytes);
+	buf[bytes] = '\0';
 
 	if (channel)
 		ret = iiod->ops->ch_write_attr(device, channel, ch_out,
